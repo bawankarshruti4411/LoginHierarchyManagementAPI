bfc21ffb37a0f22ce679ab083e856be2b689605d
Shruti Bawankar <bawankarshruti02@gmail.com>
2026-05-23 21:09:11 +0530

    Update db.py

--- db.py
+++ db.py
@@ -4,7 +4,7 @@ import os
 def get_db():
     db = mysql.connector.connect(
         host=os.environ.get("DB_HOST"),
-        port=int(os.environ.get("DB_PORT", 3306)),
+        port=int(os.environ.get("DB_PORT", 16398)),
         user=os.environ.get("DB_USER"),
         password=os.environ.get("DB_PASSWORD"),
         database=os.environ.get("DB_NAME")
