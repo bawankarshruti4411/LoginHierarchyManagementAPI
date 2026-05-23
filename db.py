d558fc01c0345f4f30f15709846bef5949debbff
Shruti Bawankar <bawankarshruti02@gmail.com>
2026-05-23 14:15:34 +0530

    Update get_db function to use env variables
    Refactor database connection to use environment variables.

--- db.py
+++ db.py
@@ -1,11 +1,12 @@
 import mysql.connector
+import os
 
 def get_db():
     db = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="password",
-        database="hierarchy_db"
+        host=os.environ.get("DB_HOST"),
+        user=os.environ.get("DB_USER"),
+        password=os.environ.get("DB_PASSWORD"),
+        database=os.environ.get("DB_NAME")
     )
     cursor = db.cursor(dictionary=True)
     return db, cursor
