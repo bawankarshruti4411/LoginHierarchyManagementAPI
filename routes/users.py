bcc703eb64cee3eb88c453629874b2e1e237a39c
Shruti Bawankar <bawankarshruti02@gmail.com>
2026-05-25 15:31:25 +0530

    Refactor SQL queries to select specific fields

--- routes/users.py
+++ routes/users.py
@@ -1,35 +1,62 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 from db import get_db
 
 users_bp = Blueprint("users", __name__)
 
+
 @users_bp.route("/users", methods=["GET"])
 def get_users():
+
+    page = int(
+        request.args.get("page", 1)
+    )
+
+    limit = int(
+        request.args.get("limit", 20)
+    )
+
+    offset = (page - 1) * limit
+
     db, cursor = get_db()
+
     cursor.execute(
         """
-        SELECT *
+        SELECT id,
+               name,
+               email
         FROM company_users
-        """
+        LIMIT %s OFFSET %s
+        """,
+        (limit, offset)
     )
+
     users = cursor.fetchall()
+
     cursor.close()
     db.close()
+
     return jsonify(users)
 
 
 @users_bp.route("/users/<int:user_id>", methods=["GET"])
 def get_user(user_id):
+
     db, cursor = get_db()
+
     cursor.execute(
         """
-        SELECT *
+        SELECT id,
+               name,
+               email
         FROM company_users
         WHERE id=%s
         """,
         (user_id,)
     )
+
     user = cursor.fetchone()
+
     cursor.close()
     db.close()
+
     return jsonify(user)
