754de5f3b8534e4b9a2fd8d32386b52674266f29
Shruti Bawankar <bawankarshruti02@gmail.com>
2026-05-25 14:29:09 +0530

    Add CORS support to the Flask application

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_cors import CORS
 
 from routes.auth import auth_bp
 from routes.masters import masters_bp
@@ -8,6 +9,8 @@ from routes.permissions import permissions_bp
 
 app = Flask(__name__)
 
+CORS(app)
+
 app.register_blueprint(auth_bp)
 app.register_blueprint(masters_bp)
 app.register_blueprint(operations_bp)
@@ -16,9 +19,12 @@ app.register_blueprint(permissions_bp)
 
 @app.route("/")
 def home():
+
     return {
-        "message": "Hierarchy Management API Running"
+        "message":
+        "Hierarchy Management API Running"
     }
 
 if __name__ == "__main__":
+
     app.run(debug=True)
