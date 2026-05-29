89765950f03a2f6b540125ead9ed69a55c1b2199
Shruti Bawankar <bawankarshruti02@gmail.com>
2026-05-30 01:55:24 +0530

    Implement CRUD operations for masters endpoint

--- routes/masters.py
+++ routes/masters.py
@@ -1,12 +1,12 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
+
 from db import get_db
-from functools import lru_cache
 
 masters_bp = Blueprint("masters", __name__)
 
 
-@lru_cache(maxsize=1)
-def fetch_masters():
+@masters_bp.route("/masters", methods=["GET"])
+def get_masters():
 
     db, cursor = get_db()
 
@@ -14,6 +14,7 @@ def fetch_masters():
         """
         SELECT *
         FROM masters
+        ORDER BY id
         """
     )
 
@@ -22,18 +23,87 @@ def fetch_masters():
     cursor.close()
     db.close()
 
-    return tuple(
-        tuple(item.items())
-        for item in masters
+    return jsonify(masters)
+
+
+@masters_bp.route("/masters", methods=["POST"])
+def create_master():
+
+    data = request.json
+
+    db, cursor = get_db()
+
+    cursor.execute(
+        """
+        INSERT INTO masters
+        (name, description)
+        VALUES (%s,%s)
+        """,
+        (
+            data["name"],
+            data["description"]
+        )
     )
 
+    db.commit()
 
-@masters_bp.route("/masters", methods=["GET"])
-def get_masters():
+    cursor.close()
+    db.close()
 
-    data = [
-        dict(item)
-        for item in fetch_masters()
-    ]
+    return jsonify({
+        "message": "Master Created"
+    })
 
-    return jsonify(data)
+
+@masters_bp.route("/masters/<int:id>", methods=["PUT"])
+def update_master(id):
+
+    data = request.json
+
+    db, cursor = get_db()
+
+    cursor.execute(
+        """
+        UPDATE masters
+        SET name=%s,
+            description=%s
+        WHERE id=%s
+        """,
+        (
+            data["name"],
+            data["description"],
+            id
+        )
+    )
+
+    db.commit()
+
+    cursor.close()
+    db.close()
+
+    return jsonify({
+        "message": "Master Updated"
+    })
+
+
+@masters_bp.route("/masters/<int:id>", methods=["DELETE"])
+def delete_master(id):
+
+    db, cursor = get_db()
+
+    cursor.execute(
+        """
+        DELETE FROM masters
+        WHERE id=%s
+        """,
+        (id,)
+    )
+
+    db.commit()
+
+    cursor.close()
+    db.close()
+
+    return jsonify({
+        "message": "Master Deleted"
+    })
